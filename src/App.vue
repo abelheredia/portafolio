08b295e25c7035bdc7a41dfc7426a7cacab46851
abelheredia <herediabravoabel@gmail.com>
2022-11-16 15:43:25 -0500

    init page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <Header />
   <Init />
-  <About />
+  <!-- <About /> -->
   <Habilidades />
-  <Portafolio />
+  <!-- <Portafolio /> -->
   <Contacto />
   <Footer />
 </template>
@@ -13,23 +13,23 @@ import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import Contacto from "./components/Contacto.vue";
 import Habilidades from "./components/Habilidades.vue";
-import About from "./components/About.vue";
+// import About from "./components/About.vue";
 import Init from "./components/Init.vue";
-import Portafolio from "./components/Portafolio.vue";
+// import Portafolio from "./components/Portafolio.vue";
 
 export default {
   name: "App",
   created() {
-    document.title = "Abel Heredia Bravo";
+    document.title = "Abel Heredia";
   },
   components: {
     Header,
     Footer,
     Contacto,
     Habilidades,
-    About,
+    // About,
     Init,
-    Portafolio,
+    // Portafolio,
   },
 };
 </script>
--- src/components/Init.vue
+++ src/components/Init.vue
@@ -1,9 +1,66 @@
 <template>
-  <h2>Init</h2>
+  <div class="init">
+    <div class="one">
+      <div class="name">
+        <h1>Abel Heredia</h1>
+        <h3>Full Stack Developer</h3>
+      </div>
+    </div>
+    <div class="two">
+      <img src="@/assets/dev.png" alt="dev" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.init {
+  width: 100%;
+  height: calc(100vh - 4.5rem);
+  display: flex;
+}
+
+.init .one {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.init .one .name h1 {
+  margin: 0;
+}
+.init .one .name h3 {
+  color: #919191;
+  margin: 0;
+}
+
+.init .two {
+  width: 50%;
+  background: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.init .two img {
+  width: 80%;
+}
+
+@media screen and (max-width: 768px) {
+  .init {
+    flex-direction: column-reverse;
+  }
+
+  .init .one {
+    width: 100%;
+    height: 40%;
+  }
+  .init .two {
+    width: 100%;
+    height: 60%;
+  }
+}
+</style>
